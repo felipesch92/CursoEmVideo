8096657decbf28f5e29230bc71fa99325b360d46
Felipe Schmaedecke <felipe.sch92@gmail.com>
2020-12-08 15:34:34 -0300

    Laços de repetição
    Aula sobre o laço de repetição for

---
+++ aula13a.py
@@ -0,0 +1,9 @@
+for x in range(1, 7):
+    print(x, ' ', end='')
+print('')
+for x in range(3, 0, -1):
+    print(x, ' ', end='')
+print('')
+n = int(input('Digite um número: '))
+for c in range(0, n):
+    print(c, '', end='')
