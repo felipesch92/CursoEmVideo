8e9d12e5909364f148c9a82069276c08bb599aaa
Felipe Schmaedecke <felipe.sch92@gmail.com>
2021-03-29 10:07:37 -0300

    Organizado os cursos do site Curso em Vídeo
    Adicionado o curso HTML5 e CSS3, assim como organizado o curso de Python 3.

--- modulos/numeros.py
+++
@@ -1,5 +0,0 @@
-from modulos.uteis import numeros
-n = int(input('Valor: '))
-print(f'O fatorial de {n} é {numeros.fat(n)}')
-print(f'O dobro de {n} é {numeros.dobro(n)}')
-print(f'O triplo de {n} é {numeros.triplo(n)}')
